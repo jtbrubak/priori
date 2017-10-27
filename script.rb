defb845772d6a4baa97abc1d146c255a84aa777c
Jeff Brubaker <jtbrubak1@gmail.com>
2017-10-27 02:09:58 -0400

    Refactor code

--- script.rb
+++ script.rb
@@ -1,49 +1,72 @@
-records = []
-pipe_records = File.new('pipe_delimited.txt')
-comma_records = File.new('comma_delimited.txt')
-space_records = File.new('space_delimited.txt')
-pipe_records.readlines.each do |pipe_record|
-  arr = pipe_record.chomp.split(' | ')
-  new_record = {}
-  new_record['last_name'] = arr[0]
-  new_record['first_name'] = arr[1]
-  new_record['gender'] = arr[3] == "M" ? "Male" : "Female"
-  new_record['date'] = arr[5].split('-').join('/')
-  new_record['color'] = arr[4]
-  records.push(new_record)
+def run
+  records = []
+  records.concat(pipe_records)
+  records.concat(comma_records)
+  records.concat(space_records)
+  output(records)
 end
-comma_records.readlines.each do |comma_record|
-  arr = comma_record.chomp.split(', ')
-  new_record = {}
-  new_record['last_name'] = arr[0]
-  new_record['first_name'] = arr[1]
-  new_record['gender'] = arr[2]
-  new_record['date'] = arr[4]
-  new_record['color'] = arr[3]
-  records.push(new_record)
+
+def pipe_records
+  File.readlines('pipe_delimited.txt').map do |pipe_record|
+    arr = pipe_record.chomp.split(' | ')
+    {
+      last_name: arr[0],
+      first_name: arr[1],
+      gender: arr[3] == "M" ? "Male" : "Female",
+      date: arr[5].split('-').join('/'),
+      color: arr[4]
+    }
+  end
 end
-space_records.readlines.each do |space_record|
-  arr = space_record.chomp.split(' ')
-  new_record = {}
-  new_record['last_name'] = arr[0]
-  new_record['first_name'] = arr[1]
-  new_record['gender'] = arr[3] == "M" ? "Male" : "Female"
-  new_record['date'] = arr[4].split('-').join('/')
-  new_record['color'] = arr[5]
-  records.push(new_record)
+
+def comma_records
+  File.readlines('comma_delimited.txt').map do |comma_record|
+    arr = comma_record.chomp.split(', ')
+    {
+      last_name: arr[0],
+      first_name: arr[1],
+      gender: arr[2],
+      date: arr[4],
+      color: arr[3]
+    }
+  end
 end
-puts "Output 1:"
-records.sort_by { |record| [record['gender'], record['last_name']] }.each do |record|
-  puts record.values.join(' ')
+
+def space_records
+  File.readlines('space_delimited.txt').map do |space_record|
+    arr = space_record.chomp.split(' ')
+    {
+      last_name: arr[0],
+      first_name: arr[1],
+      gender: arr[3] == "M" ? "Male" : "Female",
+      date: arr[4].split('-').join('/'),
+      color: arr[5]
+    }
+  end
 end
-puts "\nOutput 2:"
-records.sort_by do |record|
-  date = record['date'].split('/')
-  [date[2], date[0], date[1]]
-end.each do |record|
-  puts record.values.join(' ')
+
+def output(records)
+  sorts = [first_sort(records), second_sort(records), third_sort(records)]
+  sorts.each_with_index do |sort, i|
+    puts "Output #{i + 1}"
+    sort.each { |record| puts record.values.join(' ') }
+    puts "\n"
+  end
 end
-puts "\nOutput 3:"
-records.sort { |a, b| b['last_name'] <=> a['last_name'] }.each do |record|
-  puts record.values.join(' ')
+
+def first_sort(records)
+  records.sort_by { |record| [record[:gender], record[:last_name]] }
 end
+
+def second_sort(records)
+  records.sort_by do |record|
+    date = record[:date].split('/')
+    [date[2], date[0], date[1]]
+  end
+end
+
+def third_sort(records)
+  records.sort { |a, b| b[:last_name] <=> a[:last_name] }
+end
+
+run
